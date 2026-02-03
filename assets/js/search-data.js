d3f999dd3b082bca4a047a7b3f6492d76b547ab6
mjohnston118 <52548020+mjohnston118@users.noreply.github.com>
2026-02-03 12:02:57 +0000

    Deploying to gh-pages from @ mjohnston118/mjohnston118@c6c524d4a91da38b5b8e002a7dc52bfb9f10eb47 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "See below for repositories for my experiments",
           section: "Navigation",
           handler: () => {
             window.location.href = "/mjohnston118/repositories/";
